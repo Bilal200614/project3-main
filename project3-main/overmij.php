35b941853a9fa8f0c3eb9e36330f1bf5a6984e36
Bilal200614 <9019098@student.zadkine.nl>
2023-05-11 10:33:55 +0200

    poging tot auto save

--- project3-main/overmij.php
+++ project3-main/overmij.php
@@ -7,15 +7,15 @@
 <link rel="website icon" type="png" href="img/Logo.png">
 <body>
 	<?php include "nav.php" ?>
-	<div class="achtergrondfoto"></div>
+	
 	<main>
-    <p> Bilal Abbou is de oprichter en maker van BARA, een kledingbedrijf dat bekend staat om zijn trendy en stijlvolle ontwerpen. <br> Geboren en opgegroeid in Marokko, heeft Bilal altijd een passie gehad voor mode en design. Nadat hij zijn studie had afgerond, besloot hij zijn droom na te jagen en een eigen kledinglijn te starten.
+    <p> Bilal Abbou is de oprichter en maker van BARA, een kledingbedrijf dat bekend staat om zijn trendy en stijlvolle ontwerpen. <br> Geboren en opgegroeid in Marokko, heeft Bilal altijd een passie gehad voor mode en design. <br> Nadat hij zijn studie had afgerond, besloot hij zijn droom na te jagen en een eigen kledinglijn te starten.
+<br>
+BARA is geïnspireerd op Bilal's Marokkaanse afkomst en combineert traditionele elementen met moderne designs.  Het bedrijf staat bekend om zijn gebruik van felle kleuren, opvallende patronen en unieke stoffen. <br> Bilal is ook toegewijd aan duurzaamheid en maakt gebruik van hoogwaardige materialen die op verantwoorde wijze zijn verkregen. <br>
 
-BARA is geïnspireerd op Bilal's Marokkaanse afkomst en combineert traditionele elementen met moderne designs. Het bedrijf staat bekend om zijn gebruik van felle kleuren, opvallende patronen en unieke stoffen. Bilal is ook toegewijd aan duurzaamheid en maakt gebruik van hoogwaardige materialen die op verantwoorde wijze zijn verkregen.
+Als oprichter en creatief directeur van BARA is Bilal nauw betrokken bij elk aspect van het bedrijf, van ontwerp tot productie. <br> Hij heeft een getalenteerd team van ontwerpers en ambachtslieden samengesteld om zijn visie te realiseren en ervoor te zorgen dat elk stuk dat het bedrijf produceert van de hoogste kwaliteit is. <br>
 
-Als oprichter en creatief directeur van BARA is Bilal nauw betrokken bij elk aspect van het bedrijf, van ontwerp tot productie. Hij heeft een getalenteerd team van ontwerpers en ambachtslieden samengesteld om zijn visie te realiseren en ervoor te zorgen dat elk stuk dat het bedrijf produceert van de hoogste kwaliteit is.
-
-Ondanks het feit dat BARA nog relatief jong is, heeft het bedrijf al veel succes en erkenning gekregen. Bilal blijft zich echter richten op het uitbreiden van het merk en het creëren van nog meer innovatieve ontwerpen die de klanten van BARA blijven verrassen en inspireren. </p>
+Ondanks het feit dat BARA nog relatief jong is, heeft het bedrijf al veel succes en erkenning gekregen. <br> Bilal blijft zich echter richten op het uitbreiden van het merk en het creëren van nog meer innovatieve ontwerpen die de klanten van BARA blijven verrassen en inspireren. </p>
 	</main>
 	<?php include 'footer.php'; ?>
 </body>
